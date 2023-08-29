3b5b9e3d03c78a5d360ef33c100ea55dc01e8db8
wwr2005 <wwr20005@gmail.com>
2023-08-29 08:57:27 +0300

    frog

--- src/Main.java
+++ src/Main.java
@@ -18,5 +18,38 @@ public class Main {
         perWeightpa = perWeightpa - 7639;
 
         System.out.println("Собака " + dogWeight + " кошка " + catWeight + " бумага " + perWeightpa);
+
+        var friend = 19;
+
+        System.out.println("друг " + friend);
+
+        friend = friend + 2;
+
+        System.out.println("друг " + friend);
+
+        friend = friend / 7;
+
+        System.out.println("друг " + friend);
+
+        var frog = 3.5;
+
+        System.out.println("лягушка " + frog);
+
+        frog = frog * 10;
+        System.out.println("лягушка " + frog);
+
+        frog = frog / 3.5;
+
+        System.out.println("лягушка " + frog);
+
+        frog = frog + 4;
+
+        System.out.println("лягушка " + frog);
+
+        var boxerOne = 78.2;
+        var boxerTwo = 82.7;
+        var result = 78.2 + 82.7;
+        System.out.println("общая масса боксеров " + result +"кг");
+
     }
     }
